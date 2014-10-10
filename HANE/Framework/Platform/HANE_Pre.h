a1d31e365315f07ff75949af4970bdf3fa4cd85f
danliznh <danliz_nh@hotmail.com>
2014-10-10 23:47:17 +0800

    change the TAB to 4 spaces

--- HANE/Framework/Platform/HANE_Pre.h
+++ HANE/Framework/Platform/HANE_Pre.h
@@ -12,15 +12,15 @@
 #ifdef WIN32
 
 #elif defined(Linux) || defined(linux)|| defined(__linux) || \
-	defined(SuSE) || defined(suse) || defined(SuSe) // Linux, SuSE platform
+    defined(SuSE) || defined(suse) || defined(SuSe) // Linux, SuSE platform
 #define HANE_LINUX
 #elif defined(sun) || defined(SUN) || defined(__sun) || defined(SOLARIS) || \
-	defined(SUN_UNIX) || defined(_SUN_UNIX)      // the SUN Unix
+    defined(SUN_UNIX) || defined(_SUN_UNIX)      // the SUN Unix
 #define HANE_SOLARIS
 #elif defined(aix) || defined(AIX) || defined(_AIX) || defined(ibm) || defined(IBM)
 #define HANE_AIX       // the IBM Unix(AIX)
 #elif defined(__hpux) ||defined(hpux) ||defined(HPUX) ||defined(_HPUX) || \
-	defined(HP_UX) || defined(_HP_UX)  // the HP Unix
+    defined(HP_UX) || defined(_HP_UX)  // the HP Unix
 #define HANE_HP_UNIX
 #else
 #error Sorry, the platform is not support
